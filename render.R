f78630ba5397790948d84cc2166dfb97b48802e7
tuhocr <tuhocr.com@email.com>
2025-11-03 13:56:01 +0700

    update vao ngay 2025-11-03 13:56:01

--- render.R
+++ render.R
@@ -1,13 +1,13 @@
 library(rmarkdown)
 
 
-# repeat{
-# 
-#   rmarkdown:::render("/home/rp1/Documents/canthosxh/test.rmd")
-# 
-#   Sys.sleep(30)
-# 
-# }
+repeat{
+
+  rmarkdown:::render("/home/rp1/Documents/canthosxh/test.rmd")
+
+  Sys.sleep(30)
+
+}
 
 
-system('/home/rp1/Documents/autocommit.sh')
+# system('/home/rp1/Documents/autocommit.sh')
