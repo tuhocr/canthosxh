234882032d1fc210c4e00da302600de09588e167
tuhocr <tuhocr.com@email.com>
2025-12-20 03:08:17 +0700

    update vao ngay 2025-12-20 03:08:17

--- render.R
+++ render.R
@@ -5,10 +5,28 @@ library(quarto)
 repeat{
   
   try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/M1_OUTPUT_TABLE_SXH.qmd"))
+  
+  try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/M2_OUTPUT_TABLE_ODICH_SXH.qmd"))
+  
+  try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/M3_OUTPUT_TABLE_TCM.qmd"))
+  
+  try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/M4_OUTPUT_TABLE_ODICH_TCM.qmd"))
+  
+  
+  
+  try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/BAOCAO_SXH.qmd"))
+  
+  try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/BAOCAO_ODICH_SXH.qmd"))
+  
+  try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/BAOCAO_TCM.qmd"))
+  
+  try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/BAOCAO_ODICH_TCM.qmd"))
+  
 
   try(quarto:::quarto_render("/home/rp1/Documents/canthosxh/index.qmd"))
+  
 
-  Sys.sleep(9000)
+  Sys.sleep(3000)
 
 }
 
